8ae85dc2bceb0b6e9496457472b663553cdfd966
keerthana.s <keerthana17jaya@gmail.com>
2016-12-29 01:46:46 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <conio.h>
-void main()
+
+int main()
 {
-    char ch;
-    printf("\n enter the character:");
-    scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-    printf("\n enter the character is vowel");
-}
-    else{
-        printf("\n the character is not vowel");
-    }
+    int i=5;
+    for(i=0;i<=5;i++)
+    printf("\n Hello");
+    scanf("%d",&i);
     getch();
     }
+
