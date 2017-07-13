5c95e4b6273f4900bf4945e37cd4b52674e6c018
keerthana.s <keerthana17jaya@gmail.com>
2017-07-13 11:27:44 +0530

    Add files via upload

---
+++ amstrong2interval.java
@@ -0,0 +1,33 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class amstrong2interval {
+	public static void main(String args[])
+    {
+        int n, nu, num=0, rem;
+        Scanner s = new Scanner(System.in);
+		
+        System.out.print("Enter any Positive Number : ");
+        n = s.nextInt();
+		
+        nu = n;
+		
+        while(nu != 0)
+        {
+            rem = nu%10;
+            num = num + rem*rem*rem;
+            nu = nu/10;
+        }
+        if(num == n)
+        {
+            System.out.print("Armstrong Number");
+        }
+        else
+        {
+            System.out.print("Not an Armstrong Number");
+            s.close();
+        }
+    }
+
+}
